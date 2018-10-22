fb78173ed6c8289159ae9a9b55e5a96dcd5d2922
Mickael Van Aelst <mva@wavenet.lan>
2018-10-22 11:56:25 +0200

    Chapitre 1 et 2 fini

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'newapp';
+  // Interpolation des strings
+  square(nbr: number): number {
+    return nbr * nbr
+  }
+  title: string = 'Mon titre';
+  nombre: number = 10;
+  bool: boolean = true;
+  obj: {} = {
+    cle: 'ma cle'
+  }
+
+  //Liaison des propriétés
+  date: string = "number";
+  btnEnabled: boolean = false;
+
+  //Liaison d'évènements
+  color: string = "red";
+  changeColor(color: string): void {
+    this.color = color
+  }
+
+  //Double data-binding
+  content: string = 'Bonjour';
+
+  //Les directives
+  display: boolean = true;
+  color2: string = '';
+
+  public users: Array<{ nom }> = [];
+
+  constructor() {
+    this.users.push({nom: "Jean"})
+    this.users.push({nom: "Julie"})
+    this.users.push({nom: "Paul"})
+  }
 }
